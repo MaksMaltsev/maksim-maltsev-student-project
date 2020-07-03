1e70f72ff8cfa9bd07bdba08c07f0d3eb7c2e531
maltsev.m <maltsev.m@dbbest.com>
2020-07-03 11:15:24 +0300

    First Task. XML parser.

--- src/main/java/console/RunConsole.java
+++ src/main/java/console/RunConsole.java
@@ -1,6 +1,8 @@
 package console;
 
 
+import db.Connection;
+import java.sql.SQLException;
 import java.util.List;
 import navigation.Searchable;
 import org.xml.sax.SAXException;
@@ -14,17 +16,20 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class RunConsole {
-    public static void main(String[] args) throws TransformerException, IOException, SAXException, ParserConfigurationException {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input xml name: ");
-        String xmlFile = in.nextLine();
-        XMLtoJava xmLtoJava = new XMLtoJava();
-        Member member = xmLtoJava.parserToJava(xmlFile);
-        System.out.print("Input xml output name: ");
-        String name = in.nextLine();
-        JavaToXML javaToXML = new JavaToXML();
-        javaToXML.saveInXML(member, name);
-        List<Member> result = member.searchInDeep("tfyt");
+    public static void main(String[] args)
+        throws TransformerException, IOException, SAXException, ParserConfigurationException, SQLException, ClassNotFoundException {
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Input xml name: ");
+//        String xmlFile = in.nextLine();
+//        XMLtoJava xmLtoJava = new XMLtoJava();
+//        Member member = xmLtoJava.parserToJava(xmlFile);
+//        System.out.print("Input xml output name: ");
+//        String name = in.nextLine();
+//        JavaToXML javaToXML = new JavaToXML();
+//        javaToXML.saveInXML(member, name);
+//        List<Member> result = member.searchInDeep("Belgian Waffles");
+        Connection connection = new Connection();
+        connection.getConnection();
 
 
     }
--- src/main/java/db/Connection.java
+++ src/main/java/db/Connection.java
@@ -1,4 +1,19 @@
 package db;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connection {
+    String user = "root";
+    String password = "program1";
+    String url = "jdbc:mysql://localhost:3306/project_schema";
+
+    public void getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        try (java.sql.Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("We are connected");
+        }
+    }
 }
